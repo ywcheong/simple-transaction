732e9ccd03c3eba3075fa3efe0443324ff793a46
ywcheong <tencise@gmail.com>
2025-06-29 14:14:10 +0900

    chore: 보안 설정에서 API 문서 엔드포인트 허용

--- src/main/kotlin/com/ywcheong/simple/transaction/security/security.filter.kt
+++ src/main/kotlin/com/ywcheong/simple/transaction/security/security.filter.kt
@@ -1,26 +1,13 @@
 package com.ywcheong.simple.transaction.security
 
-import com.ywcheong.simple.transaction.member.Member
-import com.ywcheong.simple.transaction.member.MemberId
-import com.ywcheong.simple.transaction.member.MemberRepository
-import jakarta.servlet.FilterChain
-import jakarta.servlet.http.HttpServletRequest
-import jakarta.servlet.http.HttpServletResponse
+import com.ywcheong.simple.transaction.security.jwt.JwtAuthFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.core.GrantedAuthority
-import org.springframework.security.core.authority.SimpleGrantedAuthority
-import org.springframework.security.core.userdetails.UserDetails
-import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import org.springframework.stereotype.Component
-import org.springframework.stereotype.Service
-import org.springframework.web.filter.OncePerRequestFilter
 
 @Configuration
 @EnableWebSecurity
@@ -32,7 +19,7 @@ class SecurityConfig(
         csrf { it.disable() }
         sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
         authorizeHttpRequests {
-            it.requestMatchers("/members", "/members/*/tokens").permitAll()
+            it.requestMatchers("/members", "/members/tokens", "/help/**").permitAll()
             it.anyRequest().authenticated()
         }
         addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
