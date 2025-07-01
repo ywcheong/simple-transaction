4083691d37713c8df0a789e9986f856d8cb3c93e
ywcheong <tencise@gmail.com>
2025-07-01 15:36:01 +0900

    feat: 도메인 검증용 보조함수 작성

--- src/main/kotlin/com/ywcheong/simple/transaction/exception/exception.type.kt
+++ src/main/kotlin/com/ywcheong/simple/transaction/exception/exception.type.kt
@@ -3,3 +3,7 @@ package com.ywcheong.simple.transaction.exception
 class UserFaultException(
     override val message: String?
 ) : Exception(message)
+
+fun check_domain(condition: Boolean, message_lambda: () -> String) {
+    if (!condition) throw UserFaultException(message_lambda())
+}
