e22b07fb77aa8df21f040f895cb27b308600d718
ywcheong <tencise@gmail.com>
2025-07-04 10:31:09 +0900

    feat: Account 영속성 인프라

---
+++ src/main/kotlin/com/ywcheong/simple/transaction/account/infra/account.dao.kt
@@ -0,0 +1,102 @@
+package com.ywcheong.simple.transaction.account.infra
+
+import com.ywcheong.simple.transaction.account.domain.Account
+import com.ywcheong.simple.transaction.account.domain.AccountBalance
+import com.ywcheong.simple.transaction.account.domain.AccountId
+import com.ywcheong.simple.transaction.account.domain.AccountRepository
+import com.ywcheong.simple.transaction.member.domain.MemberId
+import org.seasar.doma.*
+import org.seasar.doma.boot.ConfigAutowireable
+import org.seasar.doma.jdbc.Result
+import org.springframework.stereotype.Repository
+
+@Entity(immutable = true)
+@Table(name = "account")
+class AccountEntity(
+    @Id val id: String, val owner: String, val balance: Long
+) {
+    constructor(account: Account) : this(
+        id = account.id.value, owner = account.owner.value, balance = account.balance.value
+    )
+
+    fun toAccount(): Account {
+        return Account(
+            id = AccountId(id),
+            owner = MemberId(owner),
+            balance = AccountBalance(balance),
+        )
+    }
+}
+
+@Dao
+@ConfigAutowireable
+interface AccountDao {
+    @Select
+    @Sql(
+        """
+        SELECT
+            *
+        FROM
+            account
+        WHERE
+            id = /* id */'NaN'
+    """
+    )
+    fun findById(id: String): AccountEntity?
+
+    @Select
+    @Sql(
+        """
+        SELECT
+            *
+        FROM
+            account
+        WHERE
+            owner = /* owner */'NaN'
+    """
+    )
+    fun findByOwner(owner: String): List<AccountEntity>
+
+    @Insert
+    fun insert(accountEntity: AccountEntity): Result<AccountEntity>
+
+    @Update
+    fun update(accountEntity: AccountEntity): Result<AccountEntity>
+
+    @Delete
+    @Sql(
+        """
+        DELETE
+        FROM
+            account
+        WHERE
+            id = /* id */'NaN'
+    """
+    )
+    fun delete(id: String): Int
+}
+
+@Repository
+class DefaultAccountRepository(
+    private val dao: AccountDao
+) : AccountRepository {
+    override fun findAccountById(accountId: AccountId): Account? = dao.findById(accountId.value)?.toAccount()
+
+    override fun findAccountByOwner(memberId: MemberId): List<Account> =
+        dao.findByOwner(memberId.value).map { it.toAccount() }
+
+    override fun insert(account: Account): Boolean {
+        val insertCount = dao.insert(AccountEntity(account)).count
+        return insertCount == 1
+    }
+
+    override fun update(account: Account): Boolean {
+        val updateCount = dao.update(AccountEntity(account)).count
+        return updateCount == 1
+    }
+
+    override fun delete(accountId: AccountId): Boolean {
+        val deleteCount = dao.delete(accountId.value)
+        return deleteCount == 1
+    }
+}
---
+++ src/main/kotlin/com/ywcheong/simple/transaction/account/infra/account.event.dao.kt
@@ -0,0 +1,105 @@
+package com.ywcheong.simple.transaction.account.infra
+
+import com.ywcheong.simple.transaction.account.domain.*
+import org.seasar.doma.*
+import org.seasar.doma.boot.ConfigAutowireable
+import org.seasar.doma.jdbc.Result
+import org.springframework.stereotype.Repository
+import java.util.*
+
+@Entity(immutable = true)
+data class AccountEventEntity(
+    @Id val id: String,
+    val type: Int,
+    val account: String? = null,
+    @Column(name = "account_from") val accountFrom: String? = null,
+    @Column(name = "account_to") val accountTo: String? = null,
+    val amount: Long? = null,
+    val reason: String? = null,
+    @Column(name = "issued_at") val issuedAt: Date,
+) {
+    // AccountEvent → Entity 생성자
+    constructor(event: AccountEvent) : this(
+        id = event.id.value,
+        type = event.type.type,
+        account = event.account?.value,
+        accountFrom = event.accountFrom?.value,
+        accountTo = event.accountTo?.value,
+        amount = event.amount?.value,
+        reason = event.reason,
+        issuedAt = event.issuedAt
+    )
+
+    // Entity → AccountEvent 변환
+    fun toAccountEvent(): AccountEvent = when (AccountEventType(type)) {
+        AccountEventType.DEPOSIT -> AccountDepositedEvent(
+            id = AccountEventId(id),
+            issuedAt = issuedAt,
+            account = AccountId(requireNotNull(account)),
+            amount = AccountBalanceChange(requireNotNull(amount))
+        )
+
+        AccountEventType.WITHDRAW -> AccountWithdrewEvent(
+            id = AccountEventId(id),
+            issuedAt = issuedAt,
+            account = AccountId(requireNotNull(account)),
+            amount = AccountBalanceChange(requireNotNull(amount))
+        )
+
+        AccountEventType.TRANSFER_ATTEMPT -> AccountTransferAttemptEvent(
+            id = AccountEventId(id),
+            issuedAt = issuedAt,
+            accountFrom = AccountId(requireNotNull(accountFrom)),
+            accountTo = AccountId(requireNotNull(accountTo)),
+            amount = AccountBalanceChange(requireNotNull(amount))
+        )
+
+        AccountEventType.TRANSFER_ACCEPT -> AccountTransferAcceptedEvent(
+            id = AccountEventId(id),
+            issuedAt = issuedAt,
+            accountFrom = AccountId(requireNotNull(accountFrom)),
+            accountTo = AccountId(requireNotNull(accountTo)),
+            amount = AccountBalanceChange(requireNotNull(amount))
+        )
+
+        AccountEventType.TRANSFER_REJECT -> AccountTransferRejectedEvent(
+            id = AccountEventId(id),
+            issuedAt = issuedAt,
+            accountFrom = AccountId(requireNotNull(accountFrom)),
+            accountTo = AccountId(requireNotNull(accountTo)),
+            amount = AccountBalanceChange(requireNotNull(amount)),
+            reason = requireNotNull(reason)
+        )
+
+        else -> throw IllegalStateException("비정상적인 Event 타입입니다. (값 = $type)")
+    }
+}
+
+
+@Dao
+@ConfigAutowireable
+interface AccountEventDao {
+    @Select
+    @Sql(
+        """
+        SELECT
+            *
+        FROM
+            account_event
+        WHERE
+            id = /* id */'NaN'
+    """
+    )
+    fun findById(id: String): AccountEventEntity?
+
+    @Insert
+    fun insert(event: AccountEventEntity): Result<AccountEventEntity>
+}
+
+@Repository
+class DefaultAccountEventRepository(
+    private val dao: AccountEventDao
+) : AccountEventRepository {
+    override fun selectById(eventId: AccountEventId): AccountEvent? = dao.findById(eventId.value)?.toAccountEvent()
+    override fun insert(event: AccountEvent): Boolean = dao.insert(AccountEventEntity(event)).count == 1
+}
